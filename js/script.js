8b0992e124ceb561ea63d193f022b4c3f45dbced
Vurnee <denchik_gureev@mail.ru>
2023-07-11 16:43:16 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 const OPERATORS = {
     '+': {
-        
+
         precedence: 1,
         operation: (a, b) => a + b,
     },
     '-': {
-        
+
         precedence: 1,
         operation: (a, b) => a - b,
     },
     '*': {
-       
+
         precedence: 2,
         operation: (a, b) => a * b,
     },
     '/': {
-        
+
         precedence: 2,
         operation: (a, b) => a / b,
     },
     '%': {
-        precedence:2,
-        operation: (a,b) => a/100*b, // процент от числа вроде бы, я хз как по другому )
+        precedence: 2,
+        operation: (a, b) => a * b / 100, // процент от числа вроде бы, я хз как по другому )
     },
     '^': {
-       precedence:3,
-        operation: (a,b) => Math.pow(a, b),
+        precedence: 3,
+        operation: (a, b) => Math.pow(a, b),
     },
     '√': {
-        precedence:3,
-        operation: (a ) => Math.pow(a,0.5),
+        precedence: 3,
+        operation: (a) => Math.sqrt(a, 0.5),
     },
     '!': {
         precedence: 2,
