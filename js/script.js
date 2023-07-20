fc41a9105741143fcc3f715e89a8bacf9a074755
Vurnee <denchik_gureev@mail.ru>
2023-07-20 08:19:07 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,131 +1,157 @@
-const OPERATORS = {
-    '+': {
+// const OPERATORS = {
+//     '+': {
 
-        precedence: 1,
-        operation: (a, b) => a + b,
-    },
-    '-': {
+//         precedence: 1,
+//         operation: (a, b) => a + b,
+//     },
+//     '-': {
 
-        precedence: 1,
-        operation: (a, b) => a - b,
-    },
-    '*':
-    {
+//         precedence: 1,
+//         operation: (a, b) => a - b,
+//     },
+//     '*':
+//     {
 
-        precedence: 2,
-        operation: (a, b) => a * b,
-    },
-    '/':
-    {
-        precedence: 2,
-        operation: (a, b) => {
-            try {
-                if (b === 0) {
-                    throw new Error("Деление на ноль не допускается");
-                }
+//         precedence: 2,
+//         operation: (a, b) => a * b,
+//     },
+//     '/':
+//     {
+//         precedence: 2,
+//         operation: (a, b) => {
+//             try {
+//                 if (b === 0) {
+//                     throw new Error("Деление на ноль не допускается");
+//                 }
 
-                return a / b;
-            }
-            catch (e) {
-                console.log('Произошла ошибка:');
-                console.log('Сообщение:', e.message);
-            }
-        },
-    },
-    '%':
-    {
+//                 return a / b;
+//             }
+//             catch (e) {
+//                 console.log('Произошла ошибка:');
+//                 console.log('Сообщение:', e.message);
+//             }
+//         },
+//     },
+//     '%':
+//     {
 
-        precedence: 2,
-        operation: (a, b) => a * (b / 100), // процент от числа вроде бы, я хз как по другому )
-    },
-    '^':
-    {
-        precedence: 3,
-        operation: (a, b) => Math.pow(a, b),
-    },
-    '√':
-    {
-        precedence: 3,
-        operation: (a) => {
-            try {
-                if (a <= 0) {
-                    throw new Error("Значения меньше единицы не допускаются");
-                }
-                return Math.sqrt(a);
-            }
-            catch (e) {
-                console.log('Произошла ошибка:');
-                console.log('Сообщение:', e.message);
-            }
-        }
-
-
-    },
-    '!':
-    {
-        precedence: 2,
-
-        operation: (a) => {
-
-            try {
-                if (!Number.isInteger(a)) {  // 
-                    throw new Error("Значение должно быть целым");
-                }
-                else if (a <= 0) {
-                    throw new Error("Значения меньше нуля не допускаются");
-                }
-                else if (a > 170) {
-                    throw new Error("Значения больше 170 не допускается");
-                }
-                let i = 1;
-                while (a) {
-                    i *= a--;
-                }
-                return i;
-            }
-            catch (e) {
-                console.log('Произошла ошибка:');
-                console.log('Сообщение:', e.message);
-            }
+//         precedence: 2,
+//         operation: (a, b) => a * (b / 100), // процент от числа вроде бы, я хз как по другому )
+//     },
+//     '^':
+//     {
+//         precedence: 3,
+//         operation: (a, b) => Math.pow(a, b),
+//     },
+//     '√':
+//     {
+//         precedence: 3,
+//         operation: (a) => {
+//             try {
+//                 if (a <= 0) {
+//                     throw new Error("Значения меньше единицы не допускаются");
+//                 }
+//                 return Math.sqrt(a);
+//             }
+//             catch (e) {
+//                 console.log('Произошла ошибка:');
+//                 console.log('Сообщение:', e.message);
+//             }
+//         }
 
 
-        }
-    }
-};
-const a = parseFloat(prompt("Введите первое число"));
-const operator = prompt("Введите оператор: ");
-const b = parseFloat(prompt("Введите второе число"));
+//     },
+//     '!':
+//     {
+//         precedence: 2,
 
-if (OPERATORS.hasOwnProperty(operator)) {  // используется для проверки того, имеет ли объект указанное свойство как собственное свойство. Это полезно для проверки того, унаследовал ли объект свойство, а не является ли он своим.
-    const result = OPERATORS[operator].operation(a, b);
-    console.log("Результат:", result);
-} else {
-    console.log("Некорректный оператор.");
-}
+//         operation: (a) => {
+
+//             try {
+//                 if (!Number.isInteger(a)) {  // 
+//                     throw new Error("Значение должно быть целым");
+//                 }
+//                 else if (a <= 0) {
+//                     throw new Error("Значения меньше нуля не допускаются");
+//                 }
+//                 else if (a > 170) {
+//                     throw new Error("Значения больше 170 не допускается");
+//                 }
+//                 let i = 1;
+//                 while (a) {
+//                     i *= a--;
+//                 }
+//                 return i;
+//             }
+//             catch (e) {
+//                 console.log('Произошла ошибка:');
+//                 console.log('Сообщение:', e.message);
+//             }
+
+
+//         }
+//     }
+// };
+// const a = parseFloat(prompt("Введите первое число"));
+// const operator = prompt("Введите оператор: ");
+// const b = parseFloat(prompt("Введите второе число"));
+
+// if (OPERATORS.hasOwnProperty(operator)) {  // используется для проверки того, имеет ли объект указанное свойство как собственное свойство. Это полезно для проверки того, унаследовал ли объект свойство, а не является ли он своим.
+//     const result = OPERATORS[operator].operation(a, b);
+//     console.log("Результат:", result);
+// } else {
+//     console.log("Некорректный оператор.");
+// }
+// function parse(f) {
+//     let result = [];
+//     let number = '';
+//     for (let i = 0; i < f.length; i++) {
+//         let znach = f[i];
+//         if (znach >= '0' && znach <= '9' || znach === '.') {
+//             number = +znach;
+//         }
+//         else if (znach === '+' || znach === '-' || znach === '*' || znach === '/' || znach === '^' || znach === '√' || znach === '!') {
+//             if (number !== '') {
+//                 result.push(parseFloat(number));
+//                 number = '';
+//             }
+//             result.push(znach);
+//         }
+//     }
+
+//     if (number !== '') {
+//         result.push(parseFloat(number));
+//     }
+
+//     return result;
+// }
+// //const varagenie =;
+// const vivod =parse(varagenie);
+// console.log(vivod);
 function parse(f) {
-    let result = [];
-    let number = '';
-    for (let i = 0; i < f.length; i++) {
-        let znach = f[i];
-        if (znach >= '0' && znach <= '9' || znach === '.') {
-            number = +znach;
-        }
-        else if (znach === '+' || znach === '-' || znach === '*' || znach === '/' || znach === '^' || znach === '√' || znach === '!') {
-            if (number !== '') {
-                result.push(parseFloat(number));
-                number = '';
-            }
-            result.push(znach);
-        }
-    }
+  let result = [];
 
-    if (number !== '') {
-        result.push(parseFloat(number));
-    }
+  
 
-    return result;
+  for (let i = 0; i < f.length; i++) {
+    let char = f[i];
+
+    if (char >= '0' && char <= '9' || char === '.') {
+      result.push(`${char} - число  `)
+
+    } else if (char === '+' || char === '-' || char === '*' || char === '/' || char === '^' || char === '%') {
+
+      result.push(`${char} - Символ  `);
+
+    } else if (char === '(' || char === ')') {
+
+      result.push(`${char} - Символ  `);
+    }
+  }
+
+  return result;
 }
-//const varagenie =;
-const vivod =parse(varagenie);
-console.log(vivod);
 
+const expression = '3*(8/4)';
+const parsedExpression = parse(expression);
+console.log(parsedExpression);
